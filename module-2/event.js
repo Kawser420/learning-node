fac0cee6e80039a8448c298e4fd76b9a55d861a3
Kawser420 <muhammadkawser202@gmail.com>
2025-06-27 05:29:19 +0600

    event-driven explore

--- module-2/event.js
+++ module-2/event.js
@@ -7,4 +7,15 @@ sch.on("ring", () => {
   console.log("Yahoo!! school class off now");
 });
 
+const oneMoreClass = new schoolBell();
+sch.on("ring", () => {
+  console.log("ringing but one more call left");
+});
+
+const broken = new schoolBell();
+sch.on("broken", () => {
+  console.log("broken the ring bell");
+});
+
 sch.emit("ring");
+sch.emit("broken");
