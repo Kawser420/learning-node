7e88db4012a991f40cd061f63748d8e4480e6e1c
Kawser420 <muhammadkawser202@gmail.com>
2025-06-26 19:44:11 +0600

    create iife global scoop

--- iife.js
+++ iife.js
@@ -1,3 +1,10 @@
-((name) => {
-  console.log(`Learning ${name}`);
-})("node");
+((require, module, __dirname, __filename) => {
+  let x = 10;
+  ((name) => {
+    let x = 10; // block scoop
+    console.log(`Learning ${name}`);
+  })("node");
+  console.log(x);
+  console.log(module);
+  console.log(__dirname);
+})(require, module, "./utils/add.js");
---
+++ iife2.js
@@ -0,0 +1,5 @@
+let x = 1000;
+console.log(global);
+console.log(module);
+console.log(__dirname);
+console.log(__filename);
