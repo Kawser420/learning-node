a5fda791e2e4e4cda2d4683db369055db649fa34
Kawser420 <muhammadkawser202@gmail.com>
2025-06-27 10:25:36 +0600

    node: file write, file read and reponsed

--- module-2/fs.js
+++ module-2/fs.js
@@ -3,6 +3,16 @@
 
 const fs = require("fs");
 
+console.log("Task - 1");
+
+const text = "I'm Learning Node.js";
+
+fs.writeFileSync("./hello.txt", text);
+
+console.log("Task - 2");
+
 const data = fs.readFileSync("./hello.txt", { encoding: "utf8" });
+
+console.log("Task - 4");
 
 console.log(data);
